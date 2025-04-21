041b17f86c298231765c21c1ca4e039bae79ed18
Tjoma Kovrigin <kovriginartjom77@gmail.com>
2025-04-21 16:20:35 +0300

    Add HtmlShowFor and HtmlInputFor and they use HtmlForInput and HtmlForSHow

---
+++ Core/Editors/HtmlInputFor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Mvc.Core.Editors;
+using System.Linq.Expressions;
+
+namespace MVC_Project.Core.Editors;
+
+public static class HtmlInputFor
+{
+    public static IHtmlContent InputFor<TModel, TResult>
+        (this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e, object htmlAttributes = null)
+        => h.ForInput(e, h.EditorFor(e, new { htmlAttributes = new { @class = "form-control" } }));
+}
