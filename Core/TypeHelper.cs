390eee133495dede6e150519ea1d1cef4fd8cb2c
Tjoma Kovrigin <kovriginartjom77@gmail.com>
2025-04-21 17:59:47 +0300

    Added TypeHelper and _Create,_Delete,_Details,_Edit use TypeHelper

---
+++ Core/TypeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace MVC_Project.Core;
+
+public static class TypeHelper
+{
+    public static string DisplayName<T>(IEnumerable<T> _, string? defaultName = null)
+        => DisplayName(typeof(T), defaultName);
+
+    public static string DisplayName(object? o, string? defaultName = null)
+        => DisplayName(o?.GetType(), defaultName);
+
+    public static string DisplayName(Type? t, string? defaultName = null)
+    {
+        DisplayNameAttribute? a = null;
+        if (t is not null)
+            a = Attribute.GetCustomAttribute(t, typeof(DisplayNameAttribute)) as DisplayNameAttribute;
+        return a?.DisplayName ?? t?.Name ?? defaultName ?? string.Empty;
+    }
+}
