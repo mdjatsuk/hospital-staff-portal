f77fb007fa9453bca02e94ac574d1a0e6ba4ced0
Jan Matvejev <jmatve@taltech.ee>
2025-04-21 18:38:05 +0300

    merge

--- Soft/Data/ApplicationDbContext.cs
+++ Soft/Data/ApplicationDbContext.cs
@@ -15,10 +15,6 @@ public class ApplicationDbContext : DbContext
     public DbSet<MVC_Project.Domain.AppointmentData> AppointmentData { get; set; } = default!;
 
 
-
     public DbSet<MVC_Project.Domain.Doctor> Doctor { get; set; } = default!;
-
     public DbSet<MVC_Project.Domain.Diagnosis> Diagnosis { get; set; } = default!;
-
-
 }
