3ede1606a8c6cb0c9280b63d7c353f0f2b03ca6c
Tjoma Kovrigin <kovriginartjom77@gmail.com>
2025-04-21 15:07:55 +0300

    Views have been added for the Doctor, and the Doctor tab is now working

--- Soft/Data/ApplicationDbContext.cs
+++ Soft/Data/ApplicationDbContext.cs
@@ -13,4 +13,6 @@ public class ApplicationDbContext : DbContext
     public DbSet<MVC_Project.Domain.Patient> Patients { get; set; } = default!;
 
     public DbSet<MVC_Project.Domain.AppointmentData> AppointmentData { get; set; } = default!;
+
+    public DbSet<MVC_Project.Domain.Doctor> Doctor { get; set; } = default!;
 }
