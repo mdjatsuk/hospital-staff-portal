f074c7c4fd5fad424c4dae69f9ffb2ab0e8a77cd
Ilja Stepanov <ilstep@taltech.ee>
2025-04-21 15:54:02 +0300

    Add HtmlForInput and HtmlForShow helpers

---
+++ Core/Editors/HtmlForInput.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvc.Core.Editors
+{
+    public static class HtmlForInput
+    {
+        public static IHtmlContent ForInput<TModel, TResult>(
+         this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e, IHtmlContent editor)
+            => h.ForShow(e, editor,
+                h.ValidationMessageFor(e, "", new { @class = "text-danger" }));
+    }
+}
+
