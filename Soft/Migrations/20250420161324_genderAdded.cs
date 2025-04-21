0d3458db398981b9e94b428ad8499846218c838d
Mark Djatšuk <madjat@taltech.ee>
2025-04-21 13:35:54 +0200

    gender property added

---
+++ Soft/Migrations/20250420161324_genderAdded.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_Project.Soft.Migrations
+{
+    /// <inheritdoc />
+    public partial class genderAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Patients",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Patients");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Patients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
