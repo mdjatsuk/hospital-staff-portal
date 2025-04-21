af6e08773683f651e22bc2f3f5a1cfa3707ce033
Tjoma Kovrigin <kovriginartjom77@gmail.com>
2025-04-21 14:24:13 +0300

    Lisatud Doctori jaoks kontroller, DoctorController kasutab BaseControllerit

---
+++ Soft/Controllers/DoctorController.cs
@@ -0,0 +1,7 @@
+﻿using MVC_Project.Domain;
+using MVC_Project.Soft.Data;
+
+namespace MVC_Project.Soft.Controllers;
+
+public class DoctorController(ApplicationDbContext c) :
+BaseController<Doctor>(c) { }
