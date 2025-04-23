8bcf0a74df24cbe601828d0b02a4896394a723a8
Mark Djatšuk <madjat@taltech.ee>
2025-04-23 18:20:20 +0300

    seeddata added to program.cs

--- Soft/Program.cs
+++ Soft/Program.cs
@@ -4,40 +4,69 @@ using MVC.Core;
 using MVC.Domain;
 using Mvc.Infra;
 using MVC.Soft.Data;
+using Mvc.Soft.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.")));
-
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddTransient<DbContext, ApplicationDbContext>();
-builder.Services.AddTransient<IAppointmentsRepo, AppointmentsRepo>();
-builder.Services.AddTransient<IDiagnosesRepo, DiagnosesRepo>();
-builder.Services.AddTransient<IDoctorsRepo, DoctorsRepo>();
-builder.Services.AddTransient<IPatientsRepo, PatientsRepo>();
-
-Services.init(builder.Services);
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.")));
+
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddTransient<DbContext, ApplicationDbContext>();
+        builder.Services.AddTransient<IAppointmentsRepo, AppointmentsRepo>();
+        builder.Services.AddTransient<IDiagnosesRepo, DiagnosesRepo>();
+        builder.Services.AddTransient<IDoctorsRepo, DoctorsRepo>();
+        builder.Services.AddTransient<IPatientsRepo, PatientsRepo>();
+
+        Services.init(builder.Services);
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+
+        app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+            .WithStaticAssets();
+
+
+        app.Run();
+    }
+
+    private static void seedData(WebApplication app)
+    {
+        Task.Run(async () => {
+            IServiceProvider? services = null;
+            try
+            {
+                using var scope = app.Services.CreateScope();
+                services = scope.ServiceProvider;
+                var db = services.GetRequiredService<ApplicationDbContext>();
+                await new DbInitializer(db).Initialize(1000);
+            }
+            catch (Exception e)
+            {
+                var logger = services?.GetRequiredService<ILogger<Program>>();
+                logger?.LogError(e, "An error occurred while seeding the database.");
+            }
+        });
+    }
 }
 
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
 
 
-app.Run();
