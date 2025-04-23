05a1cc54f43b43b6f42ac140bddcc4a1dceed832
Ilja Stepanov <ilstep@taltech.ee>
2025-04-23 17:02:57 +0300

    Uuendasin IRepo

--- Domain/IRepo.cs
+++ Domain/IRepo.cs
@@ -6,18 +6,14 @@ using System.Threading.Tasks;
 
 namespace MVC.Domain
 {
-    public interface IAppointmentDataRepo : IRepo<Appointment>;
+    public interface IAppointmentRepo : IRepo<Appointment>;
     public interface IDiagnosisRepo : IRepo<Diagnosis>;
     public interface IDoctorRepo : IRepo<Doctor>;
     public interface IPatientRepo : IRepo<Patient>;
     public interface IRepo<TObject>
     {
-        public Task<int> PageCount(byte pageSize, string? filter);
-        public Task<IEnumerable<TObject>> GetAsync(int pageIdx, byte pageSize
-            , string? orderBy = null, string? filter = null);
-        public Task<IEnumerable<TObject>> GetAsync(string propertyName, int idValue);
-        public Task<IEnumerable<TObject>> GetAsync();
-        public Task<TObject?> GetAsync(int? id);
+        public Task<IEnumerable<TObject>> GetAll();
+        public Task<TObject?> GetById(int? id);
         public Task AddAsync(TObject o);
         public Task UpdateAsync(TObject o);
         public Task DeleteAsync(int id);
