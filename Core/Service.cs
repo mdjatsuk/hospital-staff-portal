e25c71a7577ea2870f827116667b7eda559ee50d
Ilja Stepanov <ilstep@taltech.ee>
2025-04-23 11:52:49 +0300

    Add Service  to Core, IRepo to Domain, and internals access

---
+++ Core/Service.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Core
+{
+    public class Service
+    {
+        private static IServiceProvider? sp;
+        internal static void init(IServiceCollection c) => sp = c?.BuildServiceProvider();
+        public static T? Get<T>() => Get(typeof(T));
+        public static dynamic? Get(Type t) => sp?.GetRequiredService(t);
+    }
+}
---
+++ Domain/IRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Domain
+{
+    public interface IAppointmentDataRepo : IRepo<AppointmentData>;
+    public interface IDiagnosis : IRepo<Diagnosis>;
+    public interface IDoctor : IRepo<Doctor>;
+    public interface IPatient : IRepo<Patient>;
+    public interface IRepo<TObject>
+    {
+        public Task<int> PageCount(byte pageSize, string? filter);
+        public Task<IEnumerable<TObject>> GetAsync(int pageIdx, byte pageSize
+            , string? orderBy = null, string? filter = null);
+        public Task<IEnumerable<TObject>> GetAsync(string propertyName, int idValue);
+        public Task<IEnumerable<TObject>> GetAsync();
+        public Task<TObject?> GetAsync(int? id);
+        public Task AddAsync(TObject o);
+        public Task UpdateAsync(TObject o);
+        public Task DeleteAsync(int id);
+    }
+}
