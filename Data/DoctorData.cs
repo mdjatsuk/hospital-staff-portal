c2566df78e5381c1fa24d0f97baea0157715105d
Mark Djatšuk <madjat@taltech.ee>
2025-04-24 12:10:04 +0300

    doctor domain updated

--- Data/DoctorData.cs
+++ Data/DoctorData.cs
@@ -8,5 +8,6 @@ public sealed class DoctorData : EntityData<DoctorData>
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public Specialties? Specialization { get; set; }
+    public int PatientId { get; set; }
     public string? PhoneNumber { get; set; }
 }
