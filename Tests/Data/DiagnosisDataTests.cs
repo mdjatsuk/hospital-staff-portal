9a1c282d53480ca4de06fc564b3efaf3678d24e0
Tjoma Kovrigin <kovriginartjom77@gmail.com>
2025-04-29 15:28:38 +0300

    DiagnosisViewTests fixed and added SealedTests. And SealedTests controll all DataTests

--- Tests/Data/DiagnosisDataTests.cs
+++ Tests/Data/DiagnosisDataTests.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MVC.Tests.Data;
 
-[TestClass] public class DiagnosisDataTests : ClassTests<DiagnosisData, EntityData<DiagnosisData>>
+[TestClass] public class DiagnosisDataTests : SealedTests<DiagnosisData, EntityData<DiagnosisData>>
 {
     [TestInitialize] public override void Initialize()
     {
