a7458359ac6553cdcd373ef571c2547e459e918a
Tjoma Kovrigin <kovriginartjom77@gmail.com>
2025-05-08 11:51:37 +0300

    Added RecordNrGenerator

---
+++ Soft/Data/RecordNrGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace MVC.Soft.Data
+{
+    public static class RecordNrGenerator
+    {
+        private static readonly HashSet<string> _generatedNumbers = new HashSet<string>();
+        private static readonly Random _random = new Random();
+
+        public static string GenerateRecordNr()
+        {
+            string recordNumber;
+            do
+            {
+                var randomNumber = _random.Next(100, 1000);
+                recordNumber = $"#{randomNumber}";
+            }
+            while (!_generatedNumbers.Add(recordNumber));
+
+            return recordNumber;
+        }
+    }
+}
---
+++ Soft/Migrations/20250508084053_NewInit.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC.Soft.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RecordNr",
+                table: "Medicines",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecordNr",
+                table: "Medicines");
+        }
+    }
+}
