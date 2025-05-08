57b88f4a283c399d8ede75a4595778187fc8c77a
Tjoma Kovrigin <kovriginartjom77@gmail.com>
2025-05-08 10:41:25 +0300

    Added ApplicationDbContextTests and they working correctly

---
+++ Tests/Soft/Data/ApplicationDbContextTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+using MVC.Soft.Data;
+
+namespace MVC.Tests.Soft.Data
+{
+    [TestClass] public class ApplicationDbContextTests : BaseClassTests<ApplicationDbContext, IdentityDbContext>
+    {
+        protected override ApplicationDbContext createObj()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+               .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+               .Options;
+
+            return new ApplicationDbContext(options);
+        }
+        [TestMethod] public void AppointmentsTest() => isType(obj!.Appointments, typeof(DbSet<AppointmentData>));
+        [TestMethod] public void DoctorsTest() => isType(obj!.Doctors, typeof(DbSet<DoctorData>));
+        [TestMethod] public void MedicalRecordsTest() => isType(obj!.MedicalRecords, typeof(DbSet<MedicalRecordData>));
+        [TestMethod] public void MedicinesTest() => isType(obj!.Medicines, typeof(DbSet<MedicineData>));
+        [TestMethod] public void PatientsTest() => isType(obj!.Patients, typeof(DbSet<PatientData>));
+    }
+}
