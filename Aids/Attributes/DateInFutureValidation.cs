1f754fe2c6c47322324ce9bbfa73db83f6d20d40
Tjoma Kovrigin <kovriginartjom77@gmail.com>
2025-05-14 15:49:58 +0300

    Facade refactoring

---
+++ Aids/Attributes/DateInFutureValidation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Aids.Attributes;
+
+public class DateInFutureValidation : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is not DateTime date) return true;
+        return date.Date >= DateTime.Today;
+    }
+
+    public override string FormatErrorMessage(string name)
+        => $"{name} must be today or in the future.";
+}
--- Facade/DoctorView.cs
+++ Facade/DoctorView.cs
@@ -6,16 +6,18 @@ namespace MVC.Facade;
 
 [DisplayName(doctors)] public sealed class DoctorView : EntityView
 {
-    [Display(Name = firstName), Required, StringLength(nameLength, MinimumLength = minNameLength, ErrorMessage = firstNameError)]
+    [Display(Name = firstName), Required(ErrorMessage = requiredError), StringLength(nameLength, MinimumLength = minNameLength, ErrorMessage = firstNameError),
+        RegularExpression(namesEx, ErrorMessage = firstNameLetterError)]
     public string? FirstName { get; set; }
-    [Display(Name = lastName), Required, StringLength(nameLength, MinimumLength = minNameLength, ErrorMessage = lastNameError)]
+    [Display(Name = lastName), Required(ErrorMessage = requiredError), StringLength(nameLength, MinimumLength = minNameLength, ErrorMessage = lastNameError),
+        RegularExpression(namesEx, ErrorMessage = lastNameLetterError)]
     public string? LastName { get; set; }
-    [Display(Name = specializationName), Required] public Specialities? Specialization { get; set; }
+    [Display(Name = specializationName), Required(ErrorMessage = requiredError)] public Specialities? Specialization { get; set; }
 
-    [Display(Name = phoneNumberName), Required, Range(50000000, 59999999, ErrorMessage = phoneNumberError)]
+    [Display(Name = phoneNumberName), Required(ErrorMessage = requiredError), Range(50000000, 59999999, ErrorMessage = phoneNumberError)]
     public long? PhoneNumber { get; set; }
 
-    [Display(Name = emailName), Required, EmailValidation]
+    [Display(Name = emailName), Required(ErrorMessage = requiredError), EmailValidation]
     public string? EmailAddress { get; set; }
     public string FullName => $"{FirstName} {LastName}".Trim();
 
