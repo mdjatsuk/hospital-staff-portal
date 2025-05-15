902a8262e80dbc97102865a86fc6f88a29cb0597
Tjoma Kovrigin <kovriginartjom77@gmail.com>
2025-05-15 11:45:41 +0300

    Did clean code

--- Soft/Data/EmailGenerator.cs
+++ Soft/Data/EmailGenerator.cs
@@ -1,23 +1,22 @@
 ﻿using MVC.Aids.Attributes;
 
-namespace MVC.Soft.Data
+namespace MVC.Soft.Data;
+
+public static class EmailGenerator
 {
-    public static class EmailGenerator
+    private static readonly Random _random = new();
+
+    public static string Generate(string? firstName, string? lastName)
     {
-        private static readonly Random _random = new();
+        if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+            return $"user{_random.Next(1000, 9999)}@{GetRandomDomain()}";
 
-        public static string Generate(string? firstName, string? lastName)
-        {
-            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
-                return $"user{_random.Next(1000, 9999)}@{GetRandomDomain()}";
+        return $"{firstName.ToLower()}.{lastName.ToLower()}@{GetRandomDomain()}";
+    }
 
-            return $"{firstName.ToLower()}.{lastName.ToLower()}@{GetRandomDomain()}";
-        }
-
-        private static string GetRandomDomain()
-        {
-            var index = _random.Next(0, EmailDomainProvider.EmailDomains.Count);
-            return EmailDomainProvider.EmailDomains[index];
-        }
+    private static string GetRandomDomain()
+    {
+        var index = _random.Next(0, EmailDomainProvider.EmailDomains.Count);
+        return EmailDomainProvider.EmailDomains[index];
     }
 }
