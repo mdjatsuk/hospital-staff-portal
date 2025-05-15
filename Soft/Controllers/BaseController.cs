0e30df89dd4ded667307dcdd9bd32a03b357f83e
Mark Djatšuk <madjat@taltech.ee>
2025-05-15 09:17:22 +0300

    login is now ON

--- Soft/Controllers/BaseController.cs
+++ Soft/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MVC.Data;
 using MVC.Domain;
@@ -7,6 +8,7 @@ using MVC.Infra;
 
 namespace MVC.Soft.Controllers;
 
+[Authorize]
 public abstract class BaseController<TObject, TData, TView>(DbContext c,
     AbstractViewFactory<TData, TView> f, Func<TData?, TObject> createObject) : Controller
     where TObject : Entity<TData> where TData : EntityData<TData>, new() where TView : EntityView, new()
