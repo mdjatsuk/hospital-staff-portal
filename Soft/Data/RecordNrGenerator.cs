39e8bdb0f07f48cc8684985093c8a0f8d4a3af58
Mark Djatšuk <madjat@taltech.ee>
2025-05-19 19:28:03 +0300

    recordnrgenerated now handles exception

--- Soft/Data/RecordNrGenerator.cs
+++ Soft/Data/RecordNrGenerator.cs
@@ -3,19 +3,26 @@
     public static class RecordNrGenerator
     {
         private static readonly HashSet<string> _generatedNumbers = new HashSet<string>();
+        private static readonly object _lock = new object();
         private static readonly Random _random = new Random();
 
         public static string GenerateRecordNr()
         {
-            string recordNumber;
-            do
+            lock (_lock)
             {
-                var randomNumber = _random.Next(100, 10000);
-                recordNumber = $"#{randomNumber}";
-            }
-            while (!_generatedNumbers.Add(recordNumber));
+                if (_generatedNumbers.Count >= 9900)
+                    throw new InvalidOperationException("All possible record numbers have been generated.");
 
-            return recordNumber;
+                string recordNumber;
+                do
+                {
+                    var randomNumber = _random.Next(100, 10000);
+                    recordNumber = $"#{randomNumber}";
+                }
+                while (!_generatedNumbers.Add(recordNumber));
+
+                return recordNumber;
+            }
         }
     }
 }
